6c112a4b8fb5087da39eb6b95c37ea252e10cb33
Aref Aslani <arefaslani@gmail.com>
2014-03-14 09:51:06 +0330

    project initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devex::Application.config.secret_key_base = '80a3d009ba9dfe2371ad406036328f02bf8fe13ae957ef7948a20aa09f716787569cb38cb696d9bf8013eb4fee8210453c07551e7032325f68ea446f8476eac4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Devex::Application.config.session_store :cookie_store, key: '_devex_session'
