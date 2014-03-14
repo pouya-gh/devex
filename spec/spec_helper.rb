533522513858e2ccd5aba481fca046ddb13b80e9
Aref Aslani <arefaslani@gmail.com>
2014-03-14 13:20:41 +0330

    install and configure rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  RSpec.configure do |config|
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.run_all_when_everything_filtered = true
+    config.filter_run :focus
+    config.order = 'random'
+    config.include FactoryGirl::Syntax::Methods
+  end
+end
+
+Spork.each_run do
+end
