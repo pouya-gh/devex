b55d3f2728a5c5a359e21331b190426346cb6bb6
Aref Aslani <arefaslani@gmail.com>
2014-03-18 21:19:27 +0330

    change default locale to farsi

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Devex
         request_specs: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -33,6 +34,6 @@ module Devex
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fa
   end
 end
