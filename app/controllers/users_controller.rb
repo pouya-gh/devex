1c9c0a968b6cc78e9a7de19766b83a72c0a32220
Aref Aslani <arefaslani@gmail.com>
2014-03-19 13:03:32 +0330

    generate simple sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    begin
+      @user.save!
+      flash[:success] = I18n.translate('register.success')
+      redirect_to @user
+    rescue ActiveRecord::RecordInvalid
+      flash.now[:danger] = I18n.translate('register.fail')
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
