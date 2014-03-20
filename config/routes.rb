15015a33cd3263cb98dbb4d3db44822a03ef55f3
Aref Aslani <arefaslani@gmail.com>
2014-03-20 18:49:40 +0330

    add simple sign out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Devex::Application.routes.draw do
   root to: "static_pages#home"
   get '/sign_up', to: 'users#new', as: 'sign_up'
   get '/sign_in', to: 'sessions#new', as: 'sign_in'
+  get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
 
   resources :users, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
