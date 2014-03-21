91376d5f733c889f13251b672a6ff30d12364b0d
Aref Aslani <arefaslani@gmail.com>
2014-03-21 22:22:11 +0330

    add admin boolean field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321084538) do
+ActiveRecord::Schema.define(version: 20140321184955) do
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140321084538) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "auth_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true
