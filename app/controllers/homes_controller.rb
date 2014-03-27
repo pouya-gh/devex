a6fbe0a739c072df69da4723d043d1818acfe369
Aref Aslani <arefaslani@gmail.com>
2014-03-27 23:58:41 +0430

    rendering posts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,11 +2,11 @@ class HomesController < ApplicationController
   def index
     if signed_in?
       @posts = Post.where(published: true).order('id DESC')
-      .page(params[:page]).per(4)
+      .page(params[:page])
       # .reverse
     else
       @posts = Post.where(published: true, pro: false).order('id DESC')
-      .page(params[:page]).per(4)
+      .page(params[:page])
       # .reverse
     end
   end
