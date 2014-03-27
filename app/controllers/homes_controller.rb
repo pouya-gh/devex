e3806e4f5984ffe543ac95234847df2462784908
Aref Aslani <arefaslani@gmail.com>
2014-03-27 16:26:16 +0430

    render posts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,9 @@
 class HomesController < ApplicationController
   def index
-    
+    if signed_in?
+      @posts = Post.where(published: true)
+    else
+      @posts = Post.where(published: true, pro: false)
+    end
   end
 end
