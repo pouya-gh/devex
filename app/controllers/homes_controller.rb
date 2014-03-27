305e98bf598acbd83452898c46d4ccae687675c3
Aref Aslani <arefaslani@gmail.com>
2014-03-27 19:59:41 +0430

    add pagination to posts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,13 @@
 class HomesController < ApplicationController
   def index
     if signed_in?
-      @posts = Post.where(published: true).reverse
+      @posts = Post.where(published: true).order('id DESC')
+      .page(params[:page]).per(4)
+      # .reverse
     else
-      @posts = Post.where(published: true, pro: false).reverse
+      @posts = Post.where(published: true, pro: false).order('id DESC')
+      .page(params[:page]).per(4)
+      # .reverse
     end
   end
 end
