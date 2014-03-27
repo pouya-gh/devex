b00208a31ead624ee96dc357cb0701ea0d6cc38b
Aref Aslani <arefaslani@gmail.com>
2014-03-27 15:13:39 +0430

    remove static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Devex::Application.routes.draw do
-  root to: "static_pages#home"
   get '/sign_up', to: 'users#new', as: 'sign_up'
   get '/sign_in', to: 'sessions#new', as: 'sign_in'
   get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
