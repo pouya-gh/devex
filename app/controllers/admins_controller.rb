1af47f6d0680bd02dc26a7609de05c1e288879e6
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-08-01 23:21:26 +0430

    add password reseting methods

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,6 +6,53 @@ class AdminsController < ApplicationController
     render layout: 'admin'
 	end
 
+  def request_token
+  end
+
+  def send_password_token
+    if(admin = Admin.find_by_email(params[:email]))
+      @link = newpass_admin_url(admin.auth_token)
+      respond_to do |format|
+        UserMailer.password_reset_url_email(admin, @link).deliver        
+        format.html
+      end
+    else
+      flash.now[:danger] = I18n.translate('register.fail')
+      render :fail
+    end
+  end
+
+  def new_password
+    if (@admin = Admin.find_by_auth_token(params[:id]))
+      @token = params[:id]
+      respond_to do |format|
+        format.html
+      end
+    else
+      flash.now[:danger] = I18n.translate('password_reset.token_not_valid')
+      render :fail
+    end
+  end
+
+  def reset_password
+    if(@admin = Admin.find_by_auth_token(params[:token])) 
+      @admin.password = params[:admin][:password]
+      @admin.password_confirmation = params[:admin][:password_confirmation]
+      if @admin.save
+        flash[:success] = I18n.translate('password_reset.success')
+        sign_in @admin, "false"
+        redirect_to root_url
+      else
+        flash.now[:danger] = I18n.translate('password_reset.fail')
+        @token = @admin.auth_token
+        render :new_password
+      end
+    else
+      flash.now[:danger] = I18n.translate('password_reset.token_not_valid')
+      render :fail
+    end   
+  end
+
 	private
 
   def check_signed_in
