c99c2a532ad5130038365dd9020dfd184ce866b5
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-08-11 15:05:18 +0430

    clean test database after every run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,3 +23,20 @@ end
 
 Spork.each_run do
 end
+
+Spec::Runner.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
