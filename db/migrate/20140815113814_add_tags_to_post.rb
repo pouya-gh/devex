24c500f0c9de5143b27676c8fdb252a170e27d47
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-08-15 16:34:52 +0430

    add tags to posts table

---
+++ db/migrate/20140815113814_add_tags_to_post.rb
@@ -0,0 +1,5 @@
+class AddTagsToPost < ActiveRecord::Migration
+  def change
+    add_column :posts, :tags, :string, array: true, default: '{}'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814094339) do
+ActiveRecord::Schema.define(version: 20140815113814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140814094339) do
     t.text     "digest"
     t.boolean  "published",  default: false
     t.integer  "admin_id"
+    t.string   "tags",       default: [],    array: true
   end
 
   add_index "posts", ["published"], name: "index_posts_on_published", using: :btree
