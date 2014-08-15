886393b795756d02b31d79beec55518e6782479c
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-08-15 15:39:22 +0430

    posts destruction depends on admins

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :admin
+  belongs_to :admin, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true
   validates :digest, presence: true
