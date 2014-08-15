3b9c307362327254a5764ef0ef3fa6825b67bb64
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-08-15 16:35:30 +0430

    add validation for tags in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,16 @@
 class Post < ActiveRecord::Base
+  attr_accessor :tags
   belongs_to :admin, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true
   validates :digest, presence: true
   validates :admin_id, presence: true
+  validate :tags_cant_be_more_than_5
+
+  private
+  def tags_cant_be_more_than_5
+    if tags && (tags.size > 5)
+      errors.add(:tags, "In valid number of tags")
+    end
+  end
 end
