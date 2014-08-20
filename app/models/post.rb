90e0002cb086e36b665edaef2ca35967b1470e8d
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-08-20 14:40:26 +0430

    fix destroy dependency for posts and admins

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessor :tags
-  belongs_to :admin, dependent: :destroy
+  belongs_to :admin
   validates :title, presence: true
   validates :body, presence: true
   validates :digest, presence: true
@@ -14,7 +14,7 @@ class Post < ActiveRecord::Base
   private
   def tags_cant_be_more_than_5
     if tags && (tags.size > 5)
-      errors.add(:tags, "In valid number of tags")
+      errors.add(:tags, "Invalid number of tags")
     end
   end
 end
