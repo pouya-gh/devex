542cde89e2c917d2123f9909227ef395db46f436
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-08-25 16:03:21 +0430

    add uniqueness to posts title validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :body, presence: true
   validates :digest, presence: true
   validates :admin_id, presence: true
