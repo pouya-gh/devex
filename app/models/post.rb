0945a4ff90b2c1f793a40c128a476ba987d974d7
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-08-25 17:52:19 +0430

    delete whitespaces from begging and end of tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,20 @@ class Post < ActiveRecord::Base
   validates :admin_id, presence: true
   validates :tags, length: {maximum: 5}
 
+  before_save :chomp_tags
+
   def subscribtion_needed?
     self.pro
   end
 
   def self.has_tag(tag)
     self.where("'#{tag}' = ANY (tags) AND published = true")
+  end
+
+  def chomp_tags
+    self.tags.size.times do |indx|
+      self.tags[indx].gsub!(/^\s+/, "")
+      self.tags[indx].gsub!(/\s+$/, "")
+    end
   end
 end
