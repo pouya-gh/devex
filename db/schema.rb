e7af20218b595cab7a308397732cf67f28a7a160
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-09-07 22:49:53 +0430

    add index to posts title

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815133255) do
+ActiveRecord::Schema.define(version: 20140907175117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140815133255) do
 
   add_index "posts", ["published"], name: "index_posts_on_published", using: :btree
   add_index "posts", ["tags"], name: "index_posts_on_tags", using: :gin
+  add_index "posts", ["title"], name: "index_posts_on_title", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "first_name"
