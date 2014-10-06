7aeb783777c1aaf4d3571d8ace955215ea2e122f
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-10-06 14:57:49 +0330

    add UsersController#show specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,12 +3,27 @@ require 'spec_helper'
 describe UsersController do
   describe "GET #show" do
     context "not signed in" do
-      it "renders sign in template"
+      it "renders sign in template" do
+        expected_url = sign_in_url(redirect_url: request.original_url)
+        get :show, id: 1
+        expect(response).to redirect_to(expected_url)
+      end
     end
     
     context "signed in" do
-      it "renders :show template if signed in"
-      it "assigns user to @user"
+      before :each do
+        @user = create(:user)
+        cookies[:auth_token] = @user.auth_token
+        get :show, id: @user
+      end
+
+      it "renders :show template if signed in" do
+        expect(response).to render_template :show
+      end
+
+      it "assigns user to @user" do
+        expect(assigns(:user)).to eq @user  
+      end
     end
   end
 
