ae22334cf6f1200032da5dd7f60a503f01205495
Pouya Gharib Pour <p.gharibpour@gmail.com>
2014-10-16 11:36:12 +0330

    drop contents column

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
   validates :title, presence: true, uniqueness: true
-  validates :body, presence: true
+  #validates :body, presence: true
   validates :digest, presence: true
   validates :admin_id, presence: true
   validates :tags, length: {maximum: 5}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911131532) do
+ActiveRecord::Schema.define(version: 20141016075540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20140911131532) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "pro",        default: false
